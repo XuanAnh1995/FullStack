6f61a5dd58a6bc6af52e186174dd43c52d96f34e
XuanAnh1995 <luuvietxuananh95@gmail.com>
2026-01-26 10:11:12 +0700

    cleanup old Session02-JS files

--- Session02-JS/anonymous_function.js
+++
@@ -1,7 +0,0 @@
-// viết hàm cộng 2 con số - anonymous function
-// const xxx = function add(a, b){ return a + b; };
-
-const xxx = function (a, b){ return a + b; };
-
-// run, call
-console.log("Anonymous function  nè, 50 + 50 = "+ xxx(50, 50));
--- Session02-JS/math.ts
+++
@@ -1,9 +0,0 @@
-// Viết hàm cộng trừ nhân chia 2 con số, style TypeScript
-
-// int add(int a, int b)
-function add(a: number, b: number): number {
-	return a + b;
-}
-
-// call, run, invoke
-console.log("30 + 70 = (by TS) " + add(30, 70));
